d038577f3f5d2a4654fa0824d5788ceee1b103d8
Berdina04 <fabriberdina@gmail.com>
2022-06-30 22:35:51 -0300

    ready for deployment

--- config/db.js
+++ config/db.js
@@ -1,21 +1,21 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('tmbd' , null, null, {
-    host : 'localhost',
-    dialect: 'postgres',
-    logging : false
-})
+// const db = new Sequelize('tmbd' , null, null, {
+//     host : 'localhost',
+//     dialect: 'postgres',
+//     logging : false
+// })
 
-// require("dotenv").config()
-// const db = new Sequelize(
-//     process.env.DATABASE_URL, {
-//         dialect: "postgres",
-//         dialectOptions: {
-//           ssl: {
-//             require: true,
-//             rejectUnauthorized: false,
-//           },
-//         },
-//       }
-// )
+require("dotenv").config()
+const db = new Sequelize(
+    process.env.DATABASE_URL, {
+        dialect: "postgres",
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
+      }
+)
 
 module.exports = db
